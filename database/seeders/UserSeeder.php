48defdc0075eafd1e2d9cf927d4b6bbc314bb281
Moch. Avin <5025221061@mhs.its.ac.id>
2024-10-12 23:33:38 +0700

    feat: seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'a@a.com',
+            'password' => bcrypt('hehe'),
+        ]);
+
+        User::factory(10)->create([
+            'password' => bcrypt('haha'),
+        ]);
+    }
+}
